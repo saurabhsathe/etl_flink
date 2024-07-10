6bed22acc0d7c847d686b2aea0120dffae0142e8
saurabhsathe <sathesaurabh30@gmail.com>
2024-07-09 21:23:28 -0700

    data streaming to postgres complete

--- Flink/src/main/java/FlinkCommerce/DataStreamJob.java
+++ Flink/src/main/java/FlinkCommerce/DataStreamJob.java
@@ -3,6 +3,10 @@ package FlinkCommerce;
 import Deserializer.JSONValueDeserializationSchema;
 import model.Transaction;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.connector.jdbc.JdbcConnectionOptions;
+import org.apache.flink.connector.jdbc.JdbcExecutionOptions;
+import org.apache.flink.connector.jdbc.JdbcSink;
+import org.apache.flink.connector.jdbc.JdbcStatementBuilder;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.streaming.api.datastream.DataStream;
@@ -12,8 +16,11 @@ public class DataStreamJob {
 
     public static void main(String[] args) throws Exception {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-
+        String username = "postgres";
+        String password = "postgres";
+        String driver =  "org.postgresql.Driver";
         String topic = "financial_transactions";
+        String host = "jdbc:postgresql://localhost:5432/postgres";
 
         KafkaSource<Transaction> source = KafkaSource.<Transaction>builder()
                 .setBootstrapServers("localhost:9092")
@@ -24,8 +31,79 @@ public class DataStreamJob {
                 .build();
 
         DataStream<Transaction> transactionStream = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka source");
+        JdbcConnectionOptions jdbcConnectionOptions = new JdbcConnectionOptions.JdbcConnectionOptionsBuilder()
+                .withDriverName(driver)
+                .withUrl(host)
+                .withUsername(username)
+                .withPassword(password).build();
+
+        JdbcExecutionOptions jdbcExecutionOptions = new JdbcExecutionOptions.Builder()
+                .withBatchIntervalMs(200)
+                .withBatchSize(1000)
+                        .withMaxRetries(5).build();
 
         transactionStream.print();
+        transactionStream.addSink(JdbcSink.sink(
+
+                "CREATE TABLE IF NOT EXISTS transactions (" +
+                        "transaction_id VARCHAR(255) PRIMARY KEY, " +
+                        "product_id VARCHAR(255), " +
+                        "product_name VARCHAR(255), " +
+                        "product_category VARCHAR(255), " +
+                        "product_price DOUBLE PRECISION, " +
+                        "product_quantity INTEGER, " +
+                        "product_brand VARCHAR(255), " +
+                        "total_amount DOUBLE PRECISION, " +
+                        "currency VARCHAR(255), " +
+                        "customer_id VARCHAR(255), " +
+                        "transaction_date TIMESTAMP, " +
+                        "payment_method VARCHAR(255) " +
+                        ")",
+                (JdbcStatementBuilder<Transaction>) (preparedStatement, transaction) -> {
+
+                },
+                jdbcExecutionOptions,
+                jdbcConnectionOptions
+        )).name("Create transactions table sink");
+
+        transactionStream.addSink(JdbcSink.sink(
+                "INSERT INTO transactions(transaction_id, product_id, product_name, product_category, product_price, " +
+                        "product_quantity, product_brand, total_amount, currency, customer_id, transaction_date, payment_method) " +
+                        "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) " +
+                        "ON CONFLICT (transaction_id) DO UPDATE SET " +
+                        "product_id = EXCLUDED.product_id, " +
+                        "product_name  = EXCLUDED.product_name, " +
+                        "product_category  = EXCLUDED.product_category, " +
+                        "product_price = EXCLUDED.product_price, " +
+                        "product_quantity = EXCLUDED.product_quantity, " +
+                        "product_brand = EXCLUDED.product_brand, " +
+                        "total_amount  = EXCLUDED.total_amount, " +
+                        "currency = EXCLUDED.currency, " +
+                        "customer_id  = EXCLUDED.customer_id, " +
+                        "transaction_date = EXCLUDED.transaction_date, " +
+                        "payment_method = EXCLUDED.payment_method " +
+                        "WHERE transactions.transaction_id = EXCLUDED.transaction_id",
+                (JdbcStatementBuilder<Transaction>) (preparedStatement, transaction) -> {
+                    preparedStatement.setString(1, transaction.getTransactionId());
+                    preparedStatement.setString(2, transaction.getProductId());
+                    preparedStatement.setString(3, transaction.getProductName());
+                    preparedStatement.setString(4, transaction.getProductCategory());
+                    preparedStatement.setDouble(5, transaction.getProductPrice());
+                    preparedStatement.setInt(6, transaction.getProductQuantity());
+                    preparedStatement.setString(7, transaction.getProductBrand());
+                    preparedStatement.setDouble(8, transaction.getTotalAmount());
+                    preparedStatement.setString(9, transaction.getCurrency());
+                    preparedStatement.setString(10, transaction.getCustomerId());
+                    preparedStatement.setTimestamp(11, transaction.getTransactionDate());
+                    preparedStatement.setString(12, transaction.getPaymentMethod());
+                },
+                jdbcExecutionOptions,
+                jdbcConnectionOptions
+        )).name("Insert into transactions table sink");
+
+
+
+
 
         env.execute("Kafka Flink Job");
     }
